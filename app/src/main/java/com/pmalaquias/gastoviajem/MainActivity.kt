92c3c700288cb27b6b3ff14814f978547d063fd5
Pedro Igor <pedrode13souza@gmail.com>
2023-06-24 10:33:10 -0300

    data Validation

--- app/src/main/java/com/pmalaquias/gastoviajem/MainActivity.kt
+++ app/src/main/java/com/pmalaquias/gastoviajem/MainActivity.kt
@@ -19,23 +19,34 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     }
 
     override fun onClick(view: View) {
-        if(view.id == R.id.button_calculate){
+        if (view.id == R.id.button_calculate) {
             calculate()
         }
     }
 
-    private fun calculate(){
+    private fun isValid(): Boolean {
+        return binding.editDistance.text.toString() != "" &&
+               binding.editPrice.text.toString() != "" &&
+               binding.editAutonomy.text.toString() != "" &&
+               binding.editAutonomy.text.toString().toFloat() != 0f
+    }
 
-        val distance = binding.editDistance.text.toString().toFloat()
-        val price = binding.editPrice.text.toString().toFloat()
-        val autonomy = binding.editAutonomy.text.toString().toFloat()
+    private fun calculate() {
 
-        val totalValue = (distance * price) / autonomy
-        val totalValueStr = "R$ ${"%.2f".format(totalValue)} "
-        binding.textTotalValue.text = totalValueStr
+        if(isValid()) {
+            val distance = binding.editDistance.text.toString().toFloat()
+            val price = binding.editPrice.text.toString().toFloat()
+            val autonomy = binding.editAutonomy.text.toString().toFloat()
+
+            val totalValue = (distance * price) / autonomy
+            val totalValueStr = "R$ ${"%.2f".format(totalValue)} "
+            binding.textTotalValue.text = totalValueStr
+        }else{
 
         // toast notification
-        //Toast.makeText(this,totalValueStr,Toast.LENGTH_LONG).show()
+        Toast.makeText(this,R.string.validation_fill_all_fields,Toast.LENGTH_LONG).show()
+
+        }
 
     }
 
