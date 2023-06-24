46482d77bc01c5cb2a5da9f82f5f1c73756ce119
Pedro Igor <pedrode13souza@gmail.com>
2023-06-23 21:24:08 -0300

    click event

--- app/src/main/java/com/pmalaquias/gastoviajem/MainActivity.kt
+++ app/src/main/java/com/pmalaquias/gastoviajem/MainActivity.kt
@@ -2,9 +2,11 @@ package com.pmalaquias.gastoviajem
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import com.pmalaquias.gastoviajem.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var binding: ActivityMainBinding
 
@@ -12,9 +14,19 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.buttonCalculate.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View) {
+        if(view.id == R.id.button_calculate){
+            calculate()
+        }
     }
 
     private fun calculate(){
-
+        // toast notification
+        Toast.makeText(this,"was clicked",Toast.LENGTH_LONG).show()
     }
+
 }
